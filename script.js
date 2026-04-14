d637bae5de68a9e83094651e2cb9a2af3aca310f
Bobdud9613 <aydenngalang@gmail.com>
2026-04-14 12:34:07 -0700

    added fun javascript

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const rolandPNG = "roland.jpg"
-const homeBTN = document.getElementById("homeBtn");
+const nameBtn = document.getElementById("nameBtn");
+const image = document.getElementById("myImage");
+const homeBtn = document.getElementById("homeBtn");
+const imageList = ["AnimeBA.webp", "middleIshy.webp", "roland.jpg"]
+
+nameBtn.addEventListener("click", () => {
+    const index = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    image.src = imageList[index];
+    image.classList.remove("hidden");
+
+    setTimeout(() => {
+        image.classList.add("hidden");
+    }, 100);
+});
