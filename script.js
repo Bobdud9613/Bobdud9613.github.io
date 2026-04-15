4ff185285751908e57ea6845cfe9cedc91ac35b5
Aydenn Galang <aydenngalang@gmail.com>
2026-04-15 12:46:40 -0700

    fix layers

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ const image = document.getElementById("myImage");
 const homeBtn = document.getElementById("homeBtn");
 const imageList = ["AnimeBA.webp", "middleIshy.webp", "roland.jpg"]
 
-nameBtn.addEventListener("mousedown", () => {
+
+
+nameBtn.addEventListener("click", () => {
     const index = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
     image.src = imageList[index];
     image.classList.remove("hidden");
 
-    // setTimeout(() => {
-    //     image.classList.add("hidden");
-    // }, 100);
-});
-
-nameBtn.addEventListener("mouseup", () => {
-    image.classList.add("hidden");
+    setTimeout(() => {
+        image.classList.add("hidden");
+    }, 100);
 });
